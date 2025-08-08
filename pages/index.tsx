ead3ad2ae918c396951aa9f6714ccf2d80458f77
timhardin1979 <tim@umm411.com>
2025-08-07 22:43:20 -0600

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>Welcome to Modern Mortgage</h1>
-      <p>This is your new Next.js site deployed with Vercel!</p>
-    </main>
-  );
-}
